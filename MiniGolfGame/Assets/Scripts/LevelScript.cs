94a1282feb4c2d2509d8e146d7bcc29e013d2bde
DanielPindel <danielpindel123@gmail.com>
2024-05-22 21:26:36 +0200

    Camera

---
+++ MiniGolfGame/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LevelScript : MonoBehaviour
+{
+    public CinemachineFreeLook freeLookCamera;
+    public float mouseSensitivity = 5f;
+    public float smoothingFactor = 2f;
+    public float decelerationFactor = 0.95f;
+
+    private float currentHorizontal;
+    private float currentVertical;
+    private float horizontalVelocity;
+    private float verticalVelocity;
+    private bool isRightMouseHeld = false;
+
+    void Start()
+    {
+        freeLookCamera.m_XAxis.m_InputAxisName = "";
+        freeLookCamera.m_YAxis.m_InputAxisName = "";
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            isRightMouseHeld = true;
+
+            float targetHorizontal = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * 100f;
+            float targetVertical = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            currentHorizontal = Mathf.SmoothDamp(currentHorizontal, targetHorizontal, ref horizontalVelocity, smoothingFactor * Time.deltaTime * 10f);
+            currentVertical = Mathf.SmoothDamp(currentVertical, targetVertical, ref verticalVelocity, smoothingFactor * Time.deltaTime);
+        }
+        else
+        {
+            isRightMouseHeld = false;
+
+            currentHorizontal *= decelerationFactor;
+            currentVertical *= decelerationFactor;
+
+            if(Mathf.Abs(currentHorizontal) < 0.01f)
+            {
+                currentHorizontal = 0f;
+            }
+            if(Mathf.Abs(currentVertical) < 0.01f)
+            {
+                currentVertical = 0f;
+            }
+        }
+        freeLookCamera.m_XAxis.Value += currentHorizontal;
+        freeLookCamera.m_YAxis.Value -= currentVertical;
+    }
+}
