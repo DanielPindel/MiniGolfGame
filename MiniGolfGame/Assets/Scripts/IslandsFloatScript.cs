7c5530c2d2ae2b7ef1ef0efc26d64ced40f35395
Byouts <mo300354@student.polsl.pl>
2024-06-25 23:07:32 +0200

    Even MORE comments

--- MiniGolfGame/Assets/Scripts/IslandsFloatScript.cs
+++ MiniGolfGame/Assets/Scripts/IslandsFloatScript.cs
@@ -5,13 +5,39 @@ using UnityEngine;
 
 public class IslandsFloatScript : MonoBehaviour
 {
+    /**
+    * A public float variable for the amount of movement per update
+    */
     public float moveBy = 0.005f;
+
+    /**
+    * A public Vector3 for storing the end position in the animation
+    */
     public Vector3 endPos;
+
+    /**
+    * A private Vector3 for storing the starting position in the animation
+    */
     Vector3 startPos;
+
+    /**
+    * A private int describing direction of the animation
+    */
     int direction = 1;
+
+    /**
+    * A private bool signifyig the change of direction of the animation
+    */
     bool changedDir;
+
+    /**
+    * A private bool signaling start of the animation
+    */
     bool isStart;
 
+    /**
+    * A member function called at the start of the scene.
+    */
     void Start()
     {
         changedDir = false;
@@ -26,6 +52,9 @@ public class IslandsFloatScript : MonoBehaviour
         endPos = new Vector3(startPos.x, startPos.y + UnityEngine.Random.Range(0.5f, 1f) * direction, startPos.z);
     }
 
+    /**
+    * A member function called every frame.
+    */
     void FixedUpdate()
     {
         Vector3 localPos = transform.localPosition;
